62c35a800685eb64511fcea88852b9cdd4c3a538
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-10-24 19:55:10 +0400

    f2

--- tabaga.js
+++ tabaga.js
@@ -4,8 +4,8 @@
 var tabaga = {};
 
 /**
- * Кросс-браузерный способ
- * получения координат курсора из события в обработчике а так же определение нажатой кнопки мыши.
+ * Кросс-браузерный способ получения координат курсора из события в обработчике
+ * а так же определение нажатой кнопки мыши.
  * 
  * @param e
  * @returns
@@ -15,16 +15,21 @@ tabaga.fixEvent = function(e) {
 	e = e || window.event;
 
 	// добавить pageX/pageY для IE
-	if ( e.pageX == null && e.clientX != null ) {
+	if (e.pageX == null && e.clientX != null) {
 		var html = document.documentElement;
 		var body = document.body;
-		e.pageX = e.clientX + (html && html.scrollLeft || body && body.scrollLeft || 0) - (html.clientLeft || 0);
-		e.pageY = e.clientY + (html && html.scrollTop || body && body.scrollTop || 0) - (html.clientTop || 0);
+		e.pageX = e.clientX
+				+ (html && html.scrollLeft || body && body.scrollLeft || 0)
+				- (html.clientLeft || 0);
+		e.pageY = e.clientY
+				+ (html && html.scrollTop || body && body.scrollTop || 0)
+				- (html.clientTop || 0);
 	}
 
 	// добавить which для IE
 	if (!e.which && e.button) {
-		e.which = e.button & 1 ? 1 : ( e.button & 2 ? 3 : ( e.button & 4 ? 2 : 0 ) );
+		e.which = e.button & 1 ? 1
+				: (e.button & 2 ? 3 : (e.button & 4 ? 2 : 0));
 	}
 
 	return e;
@@ -47,38 +52,45 @@ tabaga.fireEvent = function(el, etype) {
 };
 
 tabaga.getOffsetRect = function(elem) {
-    var box = elem.getBoundingClientRect();
- 
-    var body = document.body;
-    var docElem = document.documentElement;
- 
-    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
-    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
-    var clientTop = docElem.clientTop || body.clientTop || 0;
-    var clientLeft = docElem.clientLeft || body.clientLeft || 0;
-    var top  = box.top +  scrollTop - clientTop;
-    var left = box.left + scrollLeft - clientLeft;
- 
-    return { top: Math.round(top), left: Math.round(left) };
+	var box = elem.getBoundingClientRect();
+
+	var body = document.body;
+	var docElem = document.documentElement;
+
+	var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+	var scrollLeft = window.pageXOffset || docElem.scrollLeft
+			|| body.scrollLeft;
+	var clientTop = docElem.clientTop || body.clientTop || 0;
+	var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+	var top = box.top + scrollTop - clientTop;
+	var left = box.left + scrollLeft - clientLeft;
+
+	return {
+		top : Math.round(top),
+		left : Math.round(left)
+	};
 };
 
 tabaga.getOffsetSum = function(elem) {
-    var top=0, left=0;
-    while(elem) {
-        top = top + parseInt(elem.offsetTop);
-        left = left + parseInt(elem.offsetLeft);
-        elem = elem.offsetParent;
-    }
- 
-    return {top: top, left: left};
+	var top = 0, left = 0;
+	while (elem) {
+		top = top + parseInt(elem.offsetTop);
+		left = left + parseInt(elem.offsetLeft);
+		elem = elem.offsetParent;
+	}
+
+	return {
+		top : top,
+		left : left
+	};
 };
 
 tabaga.getOffset = function(elem) {
-    if (elem.getBoundingClientRect) {
-        return tabaga.getOffsetRect(elem);
-    } else {
-        return tabaga.getOffsetSum(elem);
-    }
+	if (elem.getBoundingClientRect) {
+		return tabaga.getOffsetRect(elem);
+	} else {
+		return tabaga.getOffsetSum(elem);
+	}
 };
 
 tabaga.emptyFalseFn = function() {
@@ -94,15 +106,42 @@ tabaga.stopEventPropagation = function(event) {
 	}
 };
 
+
+if (!String.prototype.trim) {
+	String.prototype.trim = function() {
+		return this.replace(/^\s+|\s+$/gm, '');
+	};
+}
+
+
 /**
- * Оптимизированная функция, аналог jQuery.addClass.
- * Выполняется быстрее чем jQuery.addClass
+ * Оптимизированная функция, аналог jQuery.addClass. Выполняется быстрее чем
+ * jQuery.addClass
  */
-tabaga.addClass = function(element, className) {
-	
+tabaga.addClass = function(el, value) {
+	var oldClass = ' ' + el.className + ' ';
+	if (oldClass.indexOf(' ' + value + ' ') == -1) {
+		el.className = el.className + ' ' + value;
+	}
+};
+tabaga.removeClass = function(el, value) {
+	var oldClass = ' ' + el.className + ' ';
+	var newClass = (oldClass.replace(' ' + value, '')).trim();
+	if (el.className != newClass) {
+		el.className = newClass;
+	}
 };
 
-
+tabaga.addClasses = function(el, values) {
+	var oldClass = ' ' + el.className + ' ';
+	for (var i = 0; i < className.length; i++) {
+        if (oldClass.indexOf(' ' + className[i] + ' ') == -1) {
+          append += ' ' + className[i];
+        }
+	if (oldClass.indexOf(' ' + className + ' ') == -1) {
+		node.className = node.className + ' ' + className;
+	}
+};
 
 tabaga.historyControlsMap = {};
 
@@ -111,8 +150,8 @@ tabaga.historyControlsMap = {};
  * 
  * @param anchor
  */
-tabaga.pageload = function (hash) {
-	for(var controlId in tabaga.historyControlsMap) {
+tabaga.pageload = function(hash) {
+	for ( var controlId in tabaga.historyControlsMap) {
 		var control = tabaga.historyControlsMap[controlId];
 		control.detectAnchor(hash);
 	}
@@ -138,28 +177,24 @@ $(document).ready(function() {
 //
 (function($) {
 	/**
-	 * var config = {
-		urls: {
-			feedChildNodesUrl: "pages/page",
-			feedTreeScopeNodesUrl: "pages/pageTreeScope"
-		},
-	    enableDragAndDrop: true
-	    //dragAndDropScrollContainer: null
-	}
+	 * var config = { urls: { feedChildNodesUrl: "pages/page",
+	 * feedTreeScopeNodesUrl: "pages/pageTreeScope" }, enableDragAndDrop: true
+	 * //dragAndDropScrollContainer: null }
 	 */
-	$.fn.createTreeControl = function(id, config, rootNodes) {	
+	$.fn.createTreeControl = function(id, config, rootNodes) {
 		var tree = $(this);
-		
-		config.TreeControlConstructor = config.TreeControlConstructor || tabaga.TreeControl;
+
+		config.TreeControlConstructor = config.TreeControlConstructor
+				|| tabaga.TreeControl;
 		var treeControl = new config.TreeControlConstructor(id, tree[0]);
 		treeControl.configure(config);
 		treeControl.init(rootNodes);
-		
+
 		if (!config.disableHistory) {
 			tabaga.historyControlsMap[id] = treeControl;
 		}
-		
+
 		return treeControl;
 	}
-	
+
 })(jQuery);
