fb901720ca63fef1ccf78128a8bd38a6c8feb9c8
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2014-12-25 19:51:22 +0300

    modal

--- modal/ModalCreator.js
+++ modal/ModalCreator.js
@@ -1,10 +1,45 @@
-tabaga.ModalCreator = function() {
+tabaga.ModalCreator = function(title, okCallback) {
+	this.title = title;
+	this.okCallback = okCallback;
 }
 
-tabaga.ModalCreator.prototype.onCreate = function() {
+tabaga.ModalCreator.prototype.createHeader = function(modalWindow, title) {
+	var a = document.createElement("a");
+	a.className = "modalWindow-header-close";
+	a.onclick = tabaga.modalMaster.closeModal;
+	modalWindow.appendChild(a);
+	
+	var h = document.createElement("div");
+	h.className = "modalWindow-header";
+	modalWindow.appendChild(h);
+	h.appendChild(document.createTextNode(title));
+	
+	return h;
+}
+
+tabaga.ModalCreator.prototype.createBody = function(modalWindow) {
+	var b = document.createElement("div");
+	b.className = "modalWindow-body";
+	modalWindow.appendChild(b);
+	
+	return b;
+}
+
+tabaga.ModalCreator.prototype.createFooter = function(modalWindow) {
+	var f = document.createElement("div");
+	f.className = "modalWindow-footer";
+	modalWindow.appendChild(f);
+	
+	return f;
+}
+
+tabaga.ModalCreator.prototype.onCreate = function(modalWindow) {
+	this.createHeader(modalWindow, this.title);
+	this.createBody(modalWindow);
+	this.createFooter(modalWindow);
 };
 
-tabaga.PopupMenu.prototype.onDestroy = function() {
+tabaga.ModalCreator.prototype.onDestroy = function(modalWindow) {
 };
 
 
--- modal/modalMaster.js
+++ modal/modalMaster.js
@@ -3,11 +3,55 @@
  */
 tabaga.modalMaster = (function() {
 	var modalWindowStack = [];
+	var _modalBg = null;
+	var START_ZINDEX = 1000;
+	
+	function getModalBg() {
+		if (_modalBg) {
+			return _modalBg;
+		}
+		
+		_modalBg = document.createElement("div");
+		_modalBg.className = "modalBg";
+		document.body.appendChild(_modalBg);
+		return _modalBg;
+	}
+	
 	
 	function createModalWindow(modalCreator) {
+		var mbg = getModalBg();
+		if (mbg.style.display) {
+			// set display 
+			mbg.style.display = null;
+		}
+		var z = START_ZINDEX + 2*modalWindowStack.length;
+		mbg.style.zIndex = z + "";
+		
+		var modalWindow = document.createElement("div");
+		modalWindow.className = "modalWindow";
+		modalWindow.style.zIndex = z + 1 + "";
+		document.body.appendChild(modalWindow);
+		modalCreator.onCreate(modalWindow);
+		
+		modalWindowStack.push({creator: modalCreator, modal: modalWindow});
+		
 	}
 	
 	function closeLastModal() {
+		var md = modalWindowStack.pop();
+		md.creator.onDestroy(md.modal);
+		
+		document.body.removeChild(md.modal);
+		
+		var mbg = getModalBg();
+		if (modalWindowStack.length==0) {
+			// set display 
+			mbg.style.display = 'none';
+		}
+		
+		var z = START_ZINDEX + modalWindowStack.length;
+		mbg.style.zIndex = z + "";
+		
 	}
 
 	return {
