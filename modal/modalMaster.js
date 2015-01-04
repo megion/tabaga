b955e073988ec3b19b4694b105c8faa6941de401
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2015-01-04 14:20:09 +0300

    modal master

--- modal/modalMaster.js
+++ modal/modalMaster.js
@@ -4,7 +4,8 @@
 tabaga.modalMaster = (function() {
 	var modalWindowStack = [];
 	var _modalBg = null;
-	var START_ZINDEX = 1000;
+	var START_ZINDEX = 100;
+	var START_TOP = 20;
 	
 	function getModalBg() {
 		if (_modalBg) {
@@ -17,7 +18,6 @@ tabaga.modalMaster = (function() {
 		return _modalBg;
 	}
 	
-	
 	function createModalWindow(modalCreator) {
 		var mbg = getModalBg();
 		if (mbg.style.display) {
@@ -26,15 +26,19 @@ tabaga.modalMaster = (function() {
 		}
 		var z = START_ZINDEX + 2*modalWindowStack.length;
 		mbg.style.zIndex = z + "";
+		var top = START_TOP +6*modalWindowStack.length;
 		
 		var modalWindow = document.createElement("div");
 		modalWindow.className = "modalWindow";
 		modalWindow.style.zIndex = z + 1 + "";
+		modalWindow.style.top = top + "px";
 		document.body.appendChild(modalWindow);
-		modalCreator.onCreate(modalWindow);
+		
+		var copyargs = Array.prototype.slice.call(arguments);
+		copyargs.splice(0, 1, modalWindow);
+		modalCreator.onCreate.apply(modalCreator, copyargs);
 		
 		modalWindowStack.push({creator: modalCreator, modal: modalWindow});
-		
 	}
 	
 	function closeLastModal() {
@@ -47,16 +51,16 @@ tabaga.modalMaster = (function() {
 		if (modalWindowStack.length==0) {
 			// set display 
 			mbg.style.display = 'none';
+		} else {
+		    var z = START_ZINDEX + 2*(modalWindowStack.length-1);
+		    mbg.style.zIndex = z + "";
 		}
-		
-		var z = START_ZINDEX + modalWindowStack.length;
-		mbg.style.zIndex = z + "";
 		
 	}
 
 	return {
-		openModal : function(modalCreator) {
-			createModalWindow(modalCreator);
+		openModal : function() {
+			createModalWindow.apply(this, arguments);
 		},
 		closeModal : function() {
 			closeLastModal();
